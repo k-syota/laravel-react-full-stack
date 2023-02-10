acb8e58ef87f7308725934b91fb4c95f94a9097c
Syota Nishimura <syota4723@gmail.com>
2023-02-10 16:56:05 +0900

    :sparkles: add: Signup fin

--- react/src/views/SignUp.jsx
+++ react/src/views/SignUp.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { Link} from 'react-router-dom'
 import axiosClient from '../axios-client';
 import { useStateContext } from '../contexts/ContextProvider';
@@ -9,8 +9,8 @@ export default function SignUp(){
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmationRef = useRef();
-
-  const {setUser, setToken} = useStateContext()
+  const [errors, setErrors] = useState(null);
+  const {setUser, setToken} = useStateContext();
 
   const onSubmit = (e) => {
     e.preventDefault()
@@ -20,6 +20,8 @@ export default function SignUp(){
       password: passwordRef.current.value,
       password_confirmation: passwordConfirmationRef.current.value,
     }
+
+    console.log(payLoad);
     axiosClient.post('/signup', payLoad)
     .then(({data})=>{
       setUser(data.user);
@@ -29,6 +31,7 @@ export default function SignUp(){
       const response = err.response;
       if(response && response.status === 422){
         console.log(response.data.errors);
+        setErrors(response.data.errors);
       }
     })
   }
@@ -40,6 +43,13 @@ export default function SignUp(){
               <h1 className='title'>
                 SignUp for free
               </h1>
+              {
+                errors && <div className='alert'>
+                  {Object.keys(errors).map(key=>(
+                    <p key={key}>{errors[key][0]}</p>
+                  ))}
+                </div>
+              }
               <input ref={nameRef} type="text" name="" id="" placeholder="Full Name" />
               <input ref={emailRef} type="email" name="" id="" placeholder="Email Address" />
               <input ref={passwordRef} type="password" name="" id="" placeholder="Password" />
