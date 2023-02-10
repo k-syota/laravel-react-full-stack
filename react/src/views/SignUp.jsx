63df95ca55c633cc94fdb38f18ca8d059e47febc
Syota Nishimura <syota4723@gmail.com>
2023-02-10 09:38:13 +0900

    :sparkles: Implement Signup in React

--- react/src/views/SignUp.jsx
+++ react/src/views/SignUp.jsx
@@ -1,9 +1,36 @@
+import { useRef } from 'react'
 import { Link} from 'react-router-dom'
+import axiosClient from '../axios-client';
+import { useStateContext } from '../contexts/ContextProvider';
 
 export default function SignUp(){
 
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+
+  const {setUser, setToken} = useStateContext()
+
   const onSubmit = (e) => {
     e.preventDefault()
+    const payLoad = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    }
+    axiosClient.post('/signup', payLoad)
+    .then(({data})=>{
+      setUser(data.user);
+      setToken(data.token);
+    })
+    .catch(err=>{
+      const response = err.response;
+      if(response && response.status === 422){
+        console.log(response.data.errors);
+      }
+    })
   }
 
     return(
@@ -13,10 +40,10 @@ export default function SignUp(){
               <h1 className='title'>
                 SignUp for free
               </h1>
-              <input type="text" name="" id="" placeholder="Full Name" />
-              <input type="email" name="" id="" placeholder="Email Address" />
-              <input type="password" name="" id="" placeholder="Password" />
-              <input type="password" name="" id="" placeholder="Password Confirmation" />
+              <input ref={nameRef} type="text" name="" id="" placeholder="Full Name" />
+              <input ref={emailRef} type="email" name="" id="" placeholder="Email Address" />
+              <input ref={passwordRef} type="password" name="" id="" placeholder="Password" />
+              <input ref={passwordConfirmationRef} type="password" name="" id="" placeholder="Password Confirmation" />
               <button className="btn btn-block">SingUp</button>
               <p className="message">
                 Already Registered? <Link to="/login">Sign in</Link>
