2776477e3361ceb2b7323f37cba4a8114335d753
Syota Nishimura <syota4723@gmail.com>
2023-02-12 00:51:40 +0900

    :sparkles: add: user delete actions

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom'
 import axiosClient from '../axios-client';
 
 export default function Users(){
@@ -10,12 +11,24 @@ export default function Users(){
     getUser();
   },[])
 
+  const onDelete = (u) => {
+    if(!window.confirm('Are you sure you want to delete this user?')){
+      return
+    }
+    axiosClient.delete(`/users/${u.id}`)
+    .then(()=>{
+      // TODO Notification
+      getUser();
+    })
+  }
+
   const getUser = () => {
     setLoading(true);
     axiosClient.get('/users')
     .then(({data}) => {
       setLoading(false);
       console.log(data);
+      setUsers(data.data);
     })
     .catch(()=>{
       setLoading(false)
@@ -23,8 +36,49 @@ export default function Users(){
   }
 
     return(
-        <div>
-            Users
-        </div>
+      <div>
+          <div style={{ display: "flex", justifyContent:"space-between", alignItems: "center" }}>
+            <h1>Users</h1>
+            <Link to="/users/new" className="btn-add">Add New</Link>
+          </div>
+          <div className="card animated fadeInDown">
+            <table>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Create Date</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              {
+                loading &&<tbody>
+                  <tr>
+                    <td colSpan="5" className="text-center">
+                      Loading...
+                    </td>
+                  </tr>
+                </tbody>
+              }
+              {
+                !loading &&<tbody>
+                  {users.map(u => (
+                    <tr>
+                      <td>{u.id}</td>
+                      <td>{u.name}</td>
+                      <td>{u.email}</td>
+                      <td>{u.created_at}</td>
+                      <td>
+                        <Link className="btn-edit" to={'/users/'+u.id}>Edit</Link>
+                        <button onClick={e => onDelete(u)} className="btn-delete">Delete</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              }
+            </table>
+          </div>
+      </div>
     )
 }
