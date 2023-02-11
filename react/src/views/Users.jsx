0d1d40b5cf4b5d5608209bb3b2c8665cb8f4dd80
Syota Nishimura <syota4723@gmail.com>
2023-02-11 22:30:27 +0900

    :sparkles: Users CRUD in API

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import axiosClient from '../axios-client';
+
 export default function Users(){
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getUser();
+  },[])
+
+  const getUser = () => {
+    setLoading(true);
+    axiosClient.get('/users')
+    .then(({data}) => {
+      setLoading(false);
+      console.log(data);
+    })
+    .catch(()=>{
+      setLoading(false)
+    })
+  }
+
     return(
         <div>
             Users
